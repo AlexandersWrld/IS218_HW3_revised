84ef2be999498645594be1dd8b9f7a00c9e7737b
AlexandersWrld <alexanderboydjm@gmail.com>
2024-10-16 14:41:46 -0400

    changed plugins to use calculator

--- app/plugins/multiply/__init__.py
+++ app/plugins/multiply/__init__.py
@@ -9,5 +9,5 @@ class MultiplyCommand(Command):
     def execute(self):
         number_1 = int(input('Enter your first number: '))
         number_2 = int(input('Enter your second number: '))
-        number_3 = Calculator.multiply(number_1, number_2)
+        number_3 = Calculator.multiply(number_1, number_2)       
         print(number_3) 
--- app/plugins/subtract/__init__.py
+++ app/plugins/subtract/__init__.py
@@ -10,4 +10,5 @@ class SubtractCommand(Command):
         number_1 = int(input('Enter your first number: '))
         number_2 = int(input('Enter your second number: '))
         number_3 = Calculator.subtract(number_1, number_2)
+        
         print(number_3)
